12d037a6e0709aeab9b7d7d5b8638d516d51acb2
ilya <mrlivedance@vk.com>
2023-05-01 16:22:38 +0300

    Инициализация проекта, создание директорий: src - весь код проекта, main - основной файл с кодом, utils - набор функций для работы проекта (написаны 3 функции с заглушками)

---
+++ src/utils/utils.py
@@ -0,0 +1,16 @@
+from os import path
+from json import load
+
+OPERATIONS = path.abspath('operations.json')
+
+
+def load_operations(operations_open):
+    pass
+
+
+def key_sort(operations_data):
+    pass
+
+
+def sort_operation(operations_dict):
+    pass
