dd4c58d813f515d86956cc30449fa36e7ebd10c6
ilya <mrlivedance@vk.com>
2023-05-03 15:21:52 +0300

    написана функция instance_operations которая создает экземпляры класса и складывает их в список instace_list. Написанны докстринги к функциям

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,13 @@
 from datetime import datetime
+from src.main.ClassOperations import ClassOperations
 import json
 
 
 def load_operations(operations_open=None):
+    """
+    :param operations_open: список операций в формате json
+    :return: открытый список операций
+    """
     try:
         with open(operations_open, 'r', encoding='utf-8') as f:
             file = json.load(f)
@@ -12,6 +17,9 @@ def load_operations(operations_open=None):
 
 
 def key_sort_date(operations_date):
+    """
+    Ключ для сортировки операций по дате
+    """
     try:
         return datetime.strptime(operations_date.get('date', '2019-05-19T12:51:49.023880'), "%Y-%m-%dT%H:%M:%S.%f")
     except (TypeError, FileNotFoundError, OSError):
@@ -19,6 +27,10 @@ def key_sort_date(operations_date):
 
 
 def sort_operation_date(operations_dict):
+    """
+    :param operations_dict: cписок всех операций
+    :return: отсортированный список по дате
+    """
     try:
         sort_by_date = sorted(operations_dict, key=key_sort_date, reverse=True)
         return sort_by_date
@@ -27,14 +39,37 @@ def sort_operation_date(operations_dict):
 
 
 def sort_operation_state(operations_dict):
+    """
+    :param operations_dict: отсортированный список операций по дате
+    :return: 5 последних отсортированных операций по дате и статусу
+    """
     try:
         sort_by_executed = sorted(operations_dict, key=lambda x: x.get("state") == "EXECUTED", reverse=True)
-        return sort_by_executed
+        return sort_by_executed[:5]
     except (TypeError, FileNotFoundError, OSError):
         return None
 
-# loading = load_operations(OPERATIONS)
-# print(loading)
-# sor_ = sort_operation_date(loading)
-# print(sor_)
-# print(sort_operation_state(sor_))
+
+def instance_operations(list_operations):
+    """
+
+    :param list_operations: список отсортированных операций
+    по дате и статусу выполнено
+    :return: список экземпляров класса
+    """
+    instance_list = []
+    for index, operation in enumerate(list_operations):
+        date = operation.get("date")
+        description = operation.get('description')
+        from_ = operation.get('from')
+        to_ = operation.get('to')
+        amount = operation.get('operationAmount').get('amount')
+        currency = operation.get('operationAmount').get('currency')
+        instance_list.append({index + 1: ClassOperations(date,
+                                                         description,
+                                                         from_,
+                                                         to_,
+                                                         amount,
+                                                         currency)})
+
+    return instance_list
